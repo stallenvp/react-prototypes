d3b29129587d7967807b69abee0a4ae4aa4386f5
Khaleel Younis <khaleelyounis1@gmail.com>
2018-01-19 10:04:33 -0800

    React-Intro prototype - Khaleel Younis

--- src/index.js
+++ src/index.js
@@ -1,7 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+
+const luckyNumber = () => Math.ceil(Math.random() * 1000);
+
+const greeting = (name) => {
+    let divContainer =
+        <div className="container">
+            <h1>Hello {name}, welcome to your doom!</h1>
+            <h2 className="text-muted">Your unlucky number of despair is: {luckyNumber()}</h2>
+        </div>;
+    return divContainer;
+};
+
+class User {
+    constructor(name) {
+        this.name = name;
+        this.luckyNumber = luckyNumber();
+    }
+}
+
 ReactDOM.render(
-    <h1 className="container">React Prototypes!</h1>,
+    greeting(new User('Khaleel').name),
     document.getElementById('root')
 );
